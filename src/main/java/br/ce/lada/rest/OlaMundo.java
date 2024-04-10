d272afbe20d5ab86678cf1de94ee2eda96b72471
lucasdoourado <lucas.ada95@outlook.com>
2024-04-10 17:49:07 -0300

    primeiro commit

---
+++ src/main/java/br/ce/lada/rest/OlaMundo.java
@@ -0,0 +1,13 @@
+package br.ce.lada.rest;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
+        System.out.println(response.getBody().asString().equals("Ola Mundo!"));
+        System.out.println(response.statusCode() == 200);
+    }
+}
---
+++ src/main/java/br/ce/lada/rest/OlaMundoTeste.java
@@ -0,0 +1,11 @@
+package br.ce.lada.rest;
+
+import org.junit.Test;
+
+public class OlaMundoTeste {
+
+    @Test
+    public void testOlaMundo(){
+
+    }
+}
